9d5126da04f46b41a73d830c3ef07618170f25c2
revel_sim <simon.revel@supelec.fr>
2020-03-16 20:25:46 +0100

    update

--- Preprocessing.py
+++ Preprocessing.py
@@ -6,12 +6,14 @@ import os
 path = os.getcwd()
 db_path = path + '\\MSRC_ObjCategImageDatabase_v2'  
 
+image_names = images_name = os.listdir(db_path+'/Images')[100:]
+
 print('Segmentation')
 #transformations(db_path)      
 print('Creating Graphs')
-create_graph(db_path)
+create_graph(db_path, image_names)
 print('Extracting Features')
-extract_features(db_path)
+extract_features(db_path,image_names)
 
 
     
